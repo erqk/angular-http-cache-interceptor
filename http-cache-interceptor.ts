0dde119c55c08096e7d962bcf773883c76e5815c
QianKai Er <eqiankai@hotmail.com>
2025-08-08 13:57:07 +0800

    fix: Check if response body is not null before using the cache

--- http-cache-interceptor.ts
+++ http-cache-interceptor.ts
@@ -42,7 +42,7 @@ export const httpCacheInterceptor = (options?: CacheOptions) => {
           return prevReq.data$;
         }
 
-        if (data && ttl && ttl > new Date().getTime()) {
+        if (data.body && ttl && ttl > new Date().getTime()) {
           // Use delay(0) to make response asynchronous, in order to trigger `finalize()`.
           return of(prevReq.data).pipe(delay(0));
         }
